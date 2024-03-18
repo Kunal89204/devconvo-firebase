c361d698d237a66ed4ead6114f69c288cf8176d3
Kunal89204 <Kunalkhandelwal108@gmail.com>
2024-03-18 21:21:15 +0530

    some routing

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,24 @@
 
-export default function Home() {
-  return (
-    <>
 
-    </>
-  );
+import Hero from '../../src/components/Hero'
+import Aboutus from '../../src/components/Aboutus'
+import Speakers from '../../src/components/Speakers'
+import Schedule from '../../src/components/Schedule'
+import Partners from '../../src/components/Partners'
+import Footer from '../../src/components/Footer'
+
+
+const page = () => {
+  return (
+    <div>
+    <Hero />
+    <Aboutus />
+    <Speakers />
+    <Schedule />
+    <Partners />
+    <Footer />
+  </div>
+  )
 }
+
+export default page
